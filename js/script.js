45eaf7ce136f6b9af9835bf66c2f85c8e0430efe
armsvg <52114019+armsvg@users.noreply.github.com>
2020-07-31 16:56:45 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger-menu').addEventListener('click', () => {
+  document.querySelector('.list-menu').classList.toggle('hide')
+  document.querySelector('.burger-menu').classList.toggle('active')
+})
